5178278a30cf6ab4e025ac67384cb4653ecb50cd
aumekubo <aumekubo2@gmail.com>
2023-02-19 21:32:24 -0700

    PyBank analysis - need validate Avg

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,8 +17,10 @@ import os
 import csv
 
 # The next line is doing: file_to_read = '.\\Resources\\budget_data.csv'
-file_to_read = os.path.join('..', 'Git_Personnal_Rep','Python_Challenge','Python_Mod_3_Challenge', 'PyBank', 'Resources', 'budget_data.csv')
-file_to_write = os.path.join('Python_Challenge', 'Python_Mod_3_Challenge', 'PyBank', 'Analysis', 'PyBankAnalysis.txt')
+
+file_to_read= os.path.join("Resources","budget_data.csv")
+file_to_write= os.path.join("Analysis","PyBankAnalysis.txt")
+
 
   #Assign value to the variables we are going to calculate
 
@@ -33,58 +35,68 @@ change_profit_losses_list = []
 Average_change = 0
 
 # The net total amount of "Profit/Losses" over the entire period - the calculated net PL will be added to the list below
-total = 0
+total_Net = 0
 
 # The greatest increase in profits (date and amount) over the entire period (the " " will be a placeholder for the month and year of the change, the number starts from a low value and move to a higher value)
-greatest_increase = [' ',0]
+greatest_increase = ['',0]
 
 #The greatest decrease in profits (date and amount) over the entire period (the " " will be a placeholder for the month and year of the change, the number starts from a higher value and move to a lower value)
-greatest_decrease = [' ',999999999999999999999]
+greatest_decrease = ['',999999999999999999999]
 
 # read the file to open (open with dictonary reader as it will pick up the 1st column header (keys) and add the correspondent values related to the keys (headers))
 with open(file_to_read) as budget_data:
-  reader = csv.DictReader(budget_data, delimiter=",")  
+  csvreader = csv.reader(budget_data, delimiter=',')  
+
+  header=next(csvreader)
+
+  first_row=next (csvreader)
+  total_months+=1
+  total_Net+=int(first_row[1])
+  previous_profit_losses=int(first_row[1])
+
   # after read the file header, read the rows (check the csv.file for the header names to determine the row to read)
-  for row in reader:
-    total_months = total_months + 1 #start with month 0 and add 1 as it goes through the for loop
-    total =  total + int(row['Profit/losses'])
+  for row in csvreader:
+    total_months += 1 #start with month 0 and add 1 as it goes through the for loop
+    total_Net += int(row[1])
 
 #calculate the profit-loss change by comparing the total rev. and append [] to the list above
 
-change_profit_losses=int(row['Profit/Losses'])-previous_profit_losses
-previous_profit_losses= int(row['Profit/Losses'])
-change_profit_losses_list= int(row['Profit/Losses'])+change_profit_losses_list
-month_of_change=month_of_change+(row("Date"))
+change_profit_losses=int(row[1])-previous_profit_losses
+previous_profit_losses= int(row[1])
+change_profit_losses_list+= [change_profit_losses]
+month_of_change+=[row[0]]
 
 #calculate the greatest increase Date and Amount by using an IF stmt
 
 if(change_profit_losses > greatest_increase[1]):
-  greatest_increase[0]= row["Date"]
+  greatest_increase[0]= row[0]
   greatest_increase[1]=change_profit_losses
 
 
 #calculate the greatest decrease Date and Amount by using an IF stmt and append index
 
 if(change_profit_losses < greatest_decrease[1]):
-  greatest_decrease [0]= row["Date"]
+  greatest_decrease [0]= row[0]
   greatest_decrease[1]=change_profit_losses
 
 
 #calculate the greatest increase Date and Amount by using an IF stmt
 
-Average_change = sum(change_profit_losses_list/len(change_profit_losses_list))
+Average_change = sum(change_profit_losses_list)/len(change_profit_losses_list)
 
 #create the output file
 output =(
-  f"nAnalysis\n"
-  f"Total Months:{total_months}\n"
-  f"total:(${total})\n"
-  f"Average Profit Change: ${Average_change}\n"
-  f"Greatest Increase in Profit: {greatest_increase[0]} (${greatest_increase[1]})\n"
-  f"Greatest Decrease in Profit: {greatest_decrease[0]} (${greatest_decrease[1]})\n"
+  f'\Analysis\n'
+  f'--------------------------------\n'
+  f'Total Months:{total_months}\n'
+  f'total:(${total_Net})\n'
+  f'Average Profit Change: ${Average_change}\n'
+  f'Greatest Increase in Profit: {greatest_increase[0]} (${greatest_increase[1]})\n'
+  f'Greatest Decrease in Profit: {greatest_decrease[0]} (${greatest_decrease[1]})\n'
  )
 
 print(output)
 
-with open(file_to_write,'w') as txt_file:
-  txt_file.write(output)
+with open(file_to_write,'w') as txtfile:
+  txtfile.write(output)
+  
